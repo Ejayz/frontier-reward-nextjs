1569ea9af4dae117a1fbd2c199fb56c4e9ffbcb1
Ejayz <32452006+Ejayz@users.noreply.github.com>
2023-12-04 22:39:15 +0800

    Login Update
    Update Login and Initialize UI

---
+++ app/(index)/layout.tsx
@@ -0,0 +1,21 @@
+"use client";
+import "../css/App.css";
+import "../css/signin.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="login-page">
+          <span className="top-shape"></span>
+          <span className="bottom-shape"></span>
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
