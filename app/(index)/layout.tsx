68eef6eb4bf50003ea947d4cf389ec41a9fb4220
ExerSolution <98365635+ExerSolution@users.noreply.github.com>
2023-12-06 20:35:24 +0800

    newui

--- app/(index)/layout.tsx
+++ app/(index)/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
-import "../css/App.css";
-
+import "../globals.css";
 export default function RootLayout({
   children,
 }: {
@@ -9,9 +8,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="login-page">
-          <span className="top-shape"></span>
-          <span className="bottom-shape"></span>
+       <div> 
           {children}
         </div>
       </body>
--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,43 +1,37 @@
-"use client";
-import styles from "./page.module.css";
-import { Button, Form, Image } from "react-bootstrap";
-import "../css/signin.css";
-
-export default function Home() {
-  return (
-    <div className="login-container text-center">
-      <Form className="form-signin">
-        <Image
-          className="mb-4"
-          src={"/images/logo.png"}
-          height="180"
-          alt={""}
-        />
-
-        <div className="form-fields">
-          <Form.Control
-            type="email"
-            placeholder="EMAIL"
-            onChange={(e) => {}}
-            required
-          />
-
-          <Form.Control
-            type="password"
-            placeholder="PASSWORD"
-            onChange={(e) => {}}
-            required
-          />
-
-          <Form.Control.Feedback type="invalid">
-            {0 == 0 && <div>000</div>}
-          </Form.Control.Feedback>
-
-          <Button variant="dark" type="submit">
-            LOG IN
-          </Button>
+export default function page() {
+    return (
+        <div className="login-page">
+        <span className="top-shape"></span>
+        <span className="bottom-shape"></span>
+        <div className="hero min-h-screen">
+        <div className="hero-content flex-col">
+          <div className="text-center lg:text-center">
+          
+           
+  <div className="w-60">
+    <img src="./images/logo.png" />
+  </div>
+          </div>
+          <div className="card flex-shrink-0 w-96 max-w-sm shadow-2xl bg-yellow-400">
+            <form className="card-body">
+              <div className="form-control">
+              
+                <input type="email" placeholder="Email" className="input input-bordered text-center" required />
+              </div>
+              <div className="form-control">
+               
+                <input type="password" placeholder="Password" className="input input-bordered text-center" required />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+          </div>
         </div>
-      </Form>
-    </div>
-  );
-}
+      </div>
+      </div>
+    )
+};
