6de768398f4241556527fcc14f5ce3e818501749
ExerSolution <98365635+ExerSolution@users.noreply.github.com>
2023-12-10 00:00:52 +0800

    action-modalform-val

--- app/(management)/actions/page.tsx
+++ app/(management)/actions/page.tsx
@@ -1,17 +1,106 @@
 "use client";
 import React, { useRef, useState } from "react";
-
+import { ErrorMessage, Field, Form, Formik, FormikHelpers, FormikValues } from "formik";
+import * as yup from "yup";
+import LabeledInput from "@/components/LabeledInput";
+import { Span } from "next/dist/trace";
 export default function Page() {
   const [userType, setUserType] = useState("");
   const modal = useRef<HTMLDialogElement>(null);
   const handleUserTypeChange = (event: any) => {
     setUserType(event.target.value);
   };
-
+  const initialValues = {
+    name: "",
+    description: "",
+  };
+const actionValidation = yup.object().shape({
+  name: yup.string().required("Name is required"),
+  description: yup.string().required("Description is required"),
+});
+  
   return (
     <div className="pl-10">
       {/* You can open the modal using document.getElementById('ID').showModal() method */}
-      <button
+
+      {/* The button to open modal */}
+<label htmlFor="my_modal_6" className="btn btn-primary ">Add Action</label>
+
+
+{/* Put this part before </body> tag */}
+<input type="checkbox" id="my_modal_6" className="modal-toggle" />
+<div className="modal" role="dialog">
+  <div className="modal-box">
+  <form method="dialog">
+  <label htmlFor="my_modal_6"className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 ">✕</label>
+          
+  </form>
+  <h3 className="font-bold text-lg">Add Action</h3>
+  <Formik
+  initialValues={
+    {
+      name: "",
+      description: "",
+    }
+  }
+  validationSchema={actionValidation}
+  onSubmit={(values, actions) => {
+    console.log(values);
+  }}
+  >{({ errors, touched }) => (
+    <Form>
+          <div className="form-control bg-white">
+         
+<label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <Field
+              type="text"
+              placeholder="Enter Action Name"
+              className="input input-bordered"
+              name="name"
+            /> 
+             <ErrorMessage name="name" className="flex">
+      {(msg) => (
+        <div className="text-red-600 flex">
+          <img src="../icons/warning.svg" width={20} height={20} alt="Error Icon" className="error-icon pr-1" />
+          {msg}
+        </div>
+      )}
+    </ErrorMessage>
+
+<label className="label">
+              <span className="label-text">Description</span>
+            </label>
+            <Field
+              type="text"
+              placeholder="Enter Action Description"
+              className="input input-bordered"
+              name="description"
+            /> 
+             <ErrorMessage name="description" className="flex">
+      {(msg) => (
+        <div className="text-red-600 flex">
+          <img src="../icons/warning.svg" width={20} height={20} alt="Error Icon" className="error-icon pr-1" />
+          {msg}
+        </div>
+      )}
+    </ErrorMessage>
+        {/* <ErrorMessage component="span" className="text-red-600" name="description" /> */}
+
+          </div>
+          </Form>
+  )}
+          </Formik>
+          <div className="m-8 " style={{ marginTop: 60 }}>
+            <div className="absolute bottom-6 right-6">
+              <label  htmlFor="my_modal_6" className="btn btn-neutral mr-2" >Cancel</label>
+              <button className="btn btn-primary">Submit</button>
+            </div>
+          </div>
+  </div>
+</div>
+      {/* <button
         className="btn btn-info"
         onClick={() => modal.current?.showModal()}
       >
@@ -53,7 +142,7 @@ export default function Page() {
             </div>
           </div>
         </div>
-      </dialog>
+      </dialog> */}
 
       <div className="overflow-x-auto mt-5 text-black">
         <table className="table text-base font-semibold">
