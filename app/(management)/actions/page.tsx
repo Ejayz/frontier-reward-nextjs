eed5e5b3d925e83b1dba10c6da99754cff9adfac
Ejayz <32452006+Ejayz@users.noreply.github.com>
2023-12-13 22:35:49 +0800

    Fix Error Yasser
    Fix Yasser Error

--- app/(management)/actions/page.tsx
+++ app/(management)/actions/page.tsx
@@ -1,6 +1,14 @@
 "use client";
 import React, { useRef, useState } from "react";
-import { ErrorMessage, Field, Form, Formik, FormikHelpers, FormikProps, FormikValues } from "formik";
+import {
+  ErrorMessage,
+  Field,
+  Form,
+  Formik,
+  FormikHelpers,
+  FormikProps,
+  FormikValues,
+} from "formik";
 import * as yup from "yup";
 import LabeledInput from "@/components/LabeledInput";
 import { Span } from "next/dist/trace";
@@ -19,20 +27,27 @@ export default function Page() {
     name: "",
     description: "",
   };
-const actionValidation = yup.object().shape({
-  name: yup.string().required("Name is required").matches(/^[^\d]+$/, 'Name must not include numbers'),
-  description: yup.string().required("Description is required").matches(/^[^\d]+$/, 'Name must not include numbers'),
-});
-  
+  const actionValidation = yup.object().shape({
+    name: yup
+      .string()
+      .required("Name is required")
+      .matches(/^[^\d]+$/, "Name must not include numbers"),
+    description: yup
+      .string()
+      .required("Description is required")
+      .matches(/^[^\d]+$/, "Name must not include numbers"),
+  });
 
   return (
     <div className="pl-10">
       {/* You can open the modal using document.getElementById('ID').showModal() method */}
 
       {/* The button to open modal */}
-<label htmlFor="my_modal_6" className="btn btn-primary ">Add Action</label>
+      <label htmlFor="my_modal_6" className="btn btn-primary ">
+        Add Action
+      </label>
 
-<input
+      <input
         type="checkbox"
         id="my_modal_6"
         onChange={() => {
@@ -42,109 +57,135 @@ const actionValidation = yup.object().shape({
         ref={modalAddUser}
         className="modal-toggle"
       />
-<div className="modal" role="dialog">
-  <div className="modal-box">
-  <form method="dialog">
-  <label htmlFor="my_modal_6"className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 ">✕</label>
-          
-  </form>
-  <h3 className="font-bold text-lg">Add Action</h3>
-  <Formik
-  initialValues={
-    {
-      name: "",
-      description: "",
-      created_at: 11/11/2021,
-      updated_at: 11/11/2021,
-    }
-  }
-  validationSchema={actionValidation}
-  onSubmit={async (values, { resetForm }) => {
-    console.log("Form submitted with values:", values);
-    setProcessing(true);
-    let headersList = {
-      "Accept": "*/*",
-      "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-      "Content-Type": "application/json"
-     }
-     let bodyContent = JSON.stringify({
-        "name": values.name,
-        "description": values.description,
-        "created_at": values.created_at,
-        "updated_at": values.updated_at,
-     });
-     let response = await fetch("/api/post/actions", {
-      method: "POST",
-      body: bodyContent,
-      headers: headersList,
-    });
-    
-    console.log("Response:", response);
-    
-    let data = await response.json();
-    if (data.code == 200) {
-      setProcessing(false);
-      toast.success(data.message);
-      resetForm();
-    } else {
-      setProcessing(false);
-      toast.error(data.message);
-    }
-  }}
-  >{({ errors, touched }) => (
-    <Form >
-          <div className="form-control bg-white">
-         
-<label className="label">
-              <span className="label-text text-base font-semibold">Name</span>
+      <div className="modal" role="dialog">
+        <div className="modal-box">
+          <form method="dialog">
+            <label
+              htmlFor="my_modal_6"
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 "
+            >
+              ✕
             </label>
-            <Field
-              type="text"
-              placeholder="Enter Action Name"
-              className="input input-bordered"
-              name="name"
-            /> 
-             <ErrorMessage name="name" className="flex">
-      {(msg) => (
-        <div className="text-red-600 flex">
-          <img src="../icons/warning.svg" width={20} height={20} alt="Error Icon" className="error-icon pr-1" />
-          {msg}
-        </div>
-      )}
-    </ErrorMessage>
+          </form>
+          <h3 className="font-bold text-lg">Add Action</h3>
+          <Formik
+            initialValues={{
+              name: "",
+              description: "",
+              created_at: 11 / 11 / 2021,
+              updated_at: 11 / 11 / 2021,
+            }}
+            validationSchema={actionValidation}
+            onSubmit={async (values, { resetForm }) => {
+              console.log("Form submitted with values:", values);
+              setProcessing(true);
+              let headersList = {
+                Accept: "*/*",
+                "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+                "Content-Type": "application/json",
+              };
+              let bodyContent = JSON.stringify({
+                name: values.name,
+                description: values.description,
+              });
+              let response = await fetch("/api/private/createActions/", {
+                method: "POST",
+                body: bodyContent,
+                headers: headersList,
+              });
 
-<label className="label">
-              <span className="label-text text-base font-semibold">Description</span>
-            </label>
-            <Field
-              type="text"
-              placeholder="Enter Action Description"
-              className="input input-bordered"
-              name="description"
-            /> 
-             <ErrorMessage name="description" className="flex">
-      {(msg) => (
-        <div className="text-red-600 flex">
-          <img src="../icons/warning.svg" width={20} height={20} alt="Error Icon" className="error-icon pr-1" />
-          {msg}
-        </div>
-      )}
-    </ErrorMessage>
-        {/* <ErrorMessage component="span" className="text-red-600" name="description" /> */}
+              console.log("Response:", response);
+              if (response.ok) {
+                alert("triggered and ok");
+              } else {
+                alert("triggered and not ok");
+              }
+              let data = await response.json();
+              if (data.code == 200) {
+                setProcessing(false);
+                toast.success(data.message);
+                resetForm();
+              } else {
+                setProcessing(false);
+                toast.error(data.message);
+              }
+            }}
+          >
+            {({ errors, touched }) => (
+              <Form>
+                <div className="form-control bg-white">
+                  <label className="label">
+                    <span className="label-text text-base font-semibold">
+                      Name
+                    </span>
+                  </label>
+                  <Field
+                    type="text"
+                    placeholder="Enter Action Name"
+                    className="input input-bordered"
+                    name="name"
+                  />
+                  <ErrorMessage name="name" className="flex">
+                    {(msg) => (
+                      <div className="text-red-600 flex">
+                        <img
+                          src="../icons/warning.svg"
+                          width={20}
+                          height={20}
+                          alt="Error Icon"
+                          className="error-icon pr-1"
+                        />
+                        {msg}
+                      </div>
+                    )}
+                  </ErrorMessage>
 
-          </div>
-           <div className="m-8 " style={{ marginTop: 60 }}>
-            <div className="absolute bottom-6 right-6">
-              <label  htmlFor="my_modal_6" className="btn btn-neutral mr-2" >Cancel</label>
-              <button type="submit" className="btn btn-primary">Submit</button>
-              {/* <button type="submit">Submit</button> */}
-            </div>
-          </div>
-          </Form>
-  )}
+                  <label className="label">
+                    <span className="label-text text-base font-semibold">
+                      Description
+                    </span>
+                  </label>
+                  <Field
+                    type="text"
+                    placeholder="Enter Action Description"
+                    className="input input-bordered"
+                    name="description"
+                  />
+                  <ErrorMessage name="description" className="flex">
+                    {(msg) => (
+                      <div className="text-red-600 flex">
+                        <img
+                          src="../icons/warning.svg"
+                          width={20}
+                          height={20}
+                          alt="Error Icon"
+                          className="error-icon pr-1"
+                        />
+                        {msg}
+                      </div>
+                    )}
+                  </ErrorMessage>
+                  {/* <ErrorMessage component="span" className="text-red-600" name="description" /> */}
+                </div>
+                <div className="m-8 " style={{ marginTop: 60 }}>
+                  <div className="absolute bottom-6 right-6">
+                    <label
+                      htmlFor="my_modal_6"
+                      className="btn btn-neutral mr-2"
+                    >
+                      Cancel
+                    </label>
+                    <button type="submit" className="btn btn-primary">
+                      Submit
+                    </button>
+                    {/* <button type="submit">Submit</button> */}
+                  </div>
+                </div>
+              </Form>
+            )}
           </Formik>
-    
-  </div></div>
+        </div>
+      </div>
 
       <div className="overflow-x-auto mt-5 text-black">
         <table className="table text-base font-semibold">
