f2352587105cbf76bb54c857ed46a387d5412662
ExerSolution <98365635+ExerSolution@users.noreply.github.com>
2023-12-13 21:38:30 +0800

    actionupdateapi

--- app/(management)/actions/page.tsx
+++ app/(management)/actions/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 import React, { useRef, useState } from "react";
-import { ErrorMessage, Field, Form, Formik, FormikHelpers, FormikValues } from "formik";
+import { ErrorMessage, Field, Form, Formik, FormikHelpers, FormikProps, FormikValues } from "formik";
 import * as yup from "yup";
 import LabeledInput from "@/components/LabeledInput";
 import { Span } from "next/dist/trace";
+
+import { toast } from "react-toastify";
 export default function Page() {
   const [userType, setUserType] = useState("");
+  const [processing, setProcessing] = useState(false);
+  const CustomerAccountDetail = useRef<FormikProps<any>>(null);
+  const modalAddUser = useRef<HTMLInputElement>(null);
   const modal = useRef<HTMLDialogElement>(null);
   const handleUserTypeChange = (event: any) => {
     setUserType(event.target.value);
@@ -15,10 +20,11 @@ export default function Page() {
     description: "",
   };
 const actionValidation = yup.object().shape({
-  name: yup.string().required("Name is required"),
-  description: yup.string().required("Description is required"),
+  name: yup.string().required("Name is required").matches(/^[^\d]+$/, 'Name must not include numbers'),
+  description: yup.string().required("Description is required").matches(/^[^\d]+$/, 'Name must not include numbers'),
 });
   
+
   return (
     <div className="pl-10">
       {/* You can open the modal using document.getElementById('ID').showModal() method */}
@@ -26,9 +32,16 @@ const actionValidation = yup.object().shape({
       {/* The button to open modal */}
 <label htmlFor="my_modal_6" className="btn btn-primary ">Add Action</label>
 
-
-{/* Put this part before </body> tag */}
-<input type="checkbox" id="my_modal_6" className="modal-toggle" />
+<input
+        type="checkbox"
+        id="my_modal_6"
+        onChange={() => {
+          setUserType("");
+          CustomerAccountDetail.current?.resetForm();
+        }}
+        ref={modalAddUser}
+        className="modal-toggle"
+      />
 <div className="modal" role="dialog">
   <div className="modal-box">
   <form method="dialog">
@@ -41,18 +54,49 @@ const actionValidation = yup.object().shape({
     {
       name: "",
       description: "",
+      created_at: 11/11/2021,
+      updated_at: 11/11/2021,
     }
   }
   validationSchema={actionValidation}
-  onSubmit={(values, actions) => {
-    console.log(values);
+  onSubmit={async (values, { resetForm }) => {
+    console.log("Form submitted with values:", values);
+    setProcessing(true);
+    let headersList = {
+      "Accept": "*/*",
+      "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+      "Content-Type": "application/json"
+     }
+     let bodyContent = JSON.stringify({
+        "name": values.name,
+        "description": values.description,
+        "created_at": values.created_at,
+        "updated_at": values.updated_at,
+     });
+     let response = await fetch("/api/post/actions", {
+      method: "POST",
+      body: bodyContent,
+      headers: headersList,
+    });
+    
+    console.log("Response:", response);
+    
+    let data = await response.json();
+    if (data.code == 200) {
+      setProcessing(false);
+      toast.success(data.message);
+      resetForm();
+    } else {
+      setProcessing(false);
+      toast.error(data.message);
+    }
   }}
   >{({ errors, touched }) => (
-    <Form>
+    <Form >
           <div className="form-control bg-white">
          
 <label className="label">
-              <span className="label-text">Name</span>
+              <span className="label-text text-base font-semibold">Name</span>
             </label>
             <Field
               type="text"
@@ -70,7 +114,7 @@ const actionValidation = yup.object().shape({
     </ErrorMessage>
 
 <label className="label">
-              <span className="label-text">Description</span>
+              <span className="label-text text-base font-semibold">Description</span>
             </label>
             <Field
               type="text"
@@ -89,60 +133,18 @@ const actionValidation = yup.object().shape({
         {/* <ErrorMessage component="span" className="text-red-600" name="description" /> */}
 
           </div>
+           <div className="m-8 " style={{ marginTop: 60 }}>
+            <div className="absolute bottom-6 right-6">
+              <label  htmlFor="my_modal_6" className="btn btn-neutral mr-2" >Cancel</label>
+              <button type="submit" className="btn btn-primary">Submit</button>
+              {/* <button type="submit">Submit</button> */}
+            </div>
+          </div>
           </Form>
   )}
           </Formik>
-          <div className="m-8 " style={{ marginTop: 60 }}>
-            <div className="absolute bottom-6 right-6">
-              <label  htmlFor="my_modal_6" className="btn btn-neutral mr-2" >Cancel</label>
-              <button className="btn btn-primary">Submit</button>
-            </div>
-          </div>
-  </div>
-</div>
-      {/* <button
-        className="btn btn-info"
-        onClick={() => modal.current?.showModal()}
-      >
-        Add Action
-      </button>
-      <dialog id="my_modal_3" ref={modal} className="modal">
-        <div className="modal-box">
-          <form method="dialog">
-            <button
-              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-              onClick={() => modal.current?.close()}
-            >
-              ✕
-            </button>
-          </form>
-          <h3 className="font-bold text-lg">Add Action</h3>
-          <div className="form-control bg-white">
-            <label className="label">
-              <span className="label-text">Name</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Enter Action Name"
-              className="input input-bordered"
-            />
-            <label className="label">
-              <span className="label-text">Description</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Enter Action Description"
-              className="input input-bordered"
-            />
-          </div>
-          <div className="m-8 " style={{ marginTop: 60 }}>
-            <div className="absolute bottom-6 right-6">
-              <button className="btn btn-neutral mr-2">Cancel</button>
-              <button className="btn btn-primary">Submit</button>
-            </div>
-          </div>
-        </div>
-      </dialog> */}
+    
+  </div></div>
 
       <div className="overflow-x-auto mt-5 text-black">
         <table className="table text-base font-semibold">
