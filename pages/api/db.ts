71f5b98b57ffcf01dc4fe2fd85da0e1da6e57d34
Ejayz <32452006+Ejayz@users.noreply.github.com>
2024-01-10 13:50:08 +0800

    Update DB

--- pages/api/db.ts
+++ pages/api/db.ts
@@ -1,10 +1,12 @@
 import mysql from "mysql2/promise";
-
+import *as dotenv from "dotenv"
+dotenv.config()
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 const Connection  = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password:"",
-    database: "fronteir_rewards_normed",
+    host: DB_HOST,
+    user: DB_USER,
+    password:DB_PASSWORD,
+    database: DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
