0e4305999eccbe801ca9602733660fa75734de9b
Ejayz <32452006+Ejayz@users.noreply.github.com>
2024-01-29 23:05:39 +0800

    Move/Update

---
+++ app/superadmin/settings/index.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React, {useState } from "react";
+
+
+export default function Page() {
+  const [processing, setProcessing] = useState(false);
+
+  return (
+    <div className="w-full h-full pl-10">
+      
+    </div>
+  );
+}
